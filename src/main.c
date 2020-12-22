52673e8da24195e5d9ecfed44bf035b6101db148
Danilo Lekovic <danilo@lekovic.ca>
2020-12-21 18:19:06 -0800

    Eval

--- src/main.c
+++ src/main.c
@@ -3,6 +3,33 @@
 #include <editline/readline.h>
 #include "mpc.h"
 
+long eval_op(long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    char* op = t->children[1]->contents;
+
+    long x = eval(t->children[2]);
+
+    int i = 3;
+
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+}
+
 int main(int argc, char** argv) {
     mpc_parser_t* Number = mpc_new("number");
     mpc_parser_t* Operator = mpc_new("operator");
@@ -29,7 +56,8 @@ int main(int argc, char** argv) {
         mpc_result_t r;
 
         if (mpc_parse("<stdin>", input, Stutter, &r)) {
-            mpc_ast_print(r.output);
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
         } else {
             mpc_err_print(r.error);
