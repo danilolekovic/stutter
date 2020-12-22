b688c8e1d0ccb2a119b6353aca2f5d05940f168a
Danilo Lekovic <danilo@lekovic.ca>
2020-12-21 18:29:08 -0800

    Errors

--- src/main.c
+++ src/main.c
@@ -3,22 +3,75 @@
 #include <editline/readline.h>
 #include "mpc.h"
 
-long eval_op(long x, char* op, long y) {
-    if (strcmp(op, "+") == 0) { return x + y; }
-    if (strcmp(op, "-") == 0) { return x - y; }
-    if (strcmp(op, "*") == 0) { return x * y; }
-    if (strcmp(op, "/") == 0) { return x / y; }
-    return 0;
+typedef struct {
+    int type;
+    long num;
+    int err;
+} sval;
+
+enum { SVAL_NUM, SVAL_ERR };
+enum { SERR_DIV_ZERO, SERR_BAD_OP, SERR_BAD_NUM };
+
+sval sval_num(long x) {
+    sval v;
+    v.type = SVAL_NUM;
+    v.num = x;
+    return v;
 }
 
-long eval(mpc_ast_t* t) {
+sval sval_err(int x) {
+    sval v;
+    v.type = SVAL_ERR;
+    v.err = x;
+    return v;
+}
+
+void sval_print(sval v) {
+    switch (v.type) {
+        case SVAL_NUM: printf("%li", v.num); break;
+
+        case SVAL_ERR:
+            if (v.err == SERR_DIV_ZERO) {
+                printf("Error: Division by zero!");
+            }
+            if (v.err == SERR_BAD_OP) {
+                printf("Error: Invalid operator!");
+            }
+            if (v.err == SERR_BAD_NUM) {
+                printf("Error: Invalid number!");
+            }
+        break;
+    }
+}
+
+void sval_println(sval v) { sval_print(v); putchar('\n'); }
+
+sval eval_op(sval x, char* op, sval y) {
+
+    if (x.type == SVAL_ERR) { return x; }
+    if (y.type == SVAL_ERR) { return y; }
+
+    if (strcmp(op, "+") == 0) { return sval_num(x.num + y.num); }
+    if (strcmp(op, "-") == 0) { return sval_num(x.num - y.num); }
+    if (strcmp(op, "*") == 0) { return sval_num(x.num * y.num); }
+    if (strcmp(op, "/") == 0) {
+        return y.num == 0 ? sval_err(SERR_DIV_ZERO) : sval_num(x.num / y.num);
+    }
+
+    return sval_err(SERR_BAD_OP);
+}
+
+sval eval(mpc_ast_t* t) {
     if (strstr(t->tag, "number")) {
-        return atoi(t->contents);
+        errno = 0;
+
+        long x = strtol(t->contents, NULL, 10);
+        return errno != ERANGE ? sval_num(x) : sval_err(SERR_BAD_NUM);
     }
 
     char* op = t->children[1]->contents;
 
-    long x = eval(t->children[2]);
+    sval x = eval(t->children[2]);
 
     int i = 3;
 
@@ -56,8 +109,8 @@ int main(int argc, char** argv) {
         mpc_result_t r;
 
         if (mpc_parse("<stdin>", input, Stutter, &r)) {
-            long result = eval(r.output);
-            printf("%li\n", result);
+            sval result = eval(r.output);
+            sval_println(result);
             mpc_ast_delete(r.output);
         } else {
             mpc_err_print(r.error);
